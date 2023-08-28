6dbf9b649f7b5ed9e80563cc62b3720739cb5f80
Mingy1024 <s2142717@gmail.com>
2023-08-28 11:05:01 +0800

    搜尋及網址參數綁定

--- src/pages/AlbumSearch.js
+++ src/pages/AlbumSearch.js
@@ -1,23 +1,37 @@
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import axios from 'axios';
 
 const api = 'https://api.unsplash.com/search/photos';
 const accessId = process.env.REACT_APP_UNSPLASH_ACCESS;
 
 export default function AlbumSearch() {
-  const [search, setSearch] = useState('animal');
+  const [search, setSearch] = useState('');
   const [list, setList] = useState([]);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
   useEffect(() => {
-    (async () => {
-      const response = await axios.get(
-        `${api}/?client_id=${accessId}&query=${search}`
-      );
-      const { results } = response.data;
-      console.log(results);
-      setList(results);
-    })();
+    // console.log(searchParams.get('query')); // 取出
+    // setSearchParams({query: 'building'});
+  }, []);
+
+  useEffect(() => {
+    if (search !== '') {
+      (async () => {
+        const response = await axios.get(
+          `${api}/?client_id=${accessId}&query=${search}`
+        );
+        const { results } = response.data;
+        console.log(results);
+        setList(results);
+      })();
+    }
   }, [search]);
+
+  useEffect(() => {
+    setSearch(searchParams.get('query'));
+  }, [searchParams]);
 
   return (
     <>
@@ -28,7 +42,8 @@ export default function AlbumSearch() {
         defaultValue={search}
         onKeyUp={(e) => {
           if (e.code === 'Enter') {
-            setSearch(e.target.value);
+            // setSearch(e.target.value);
+            setSearchParams({ query: e.target.value });
           }
         }}
       />
